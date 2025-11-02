cfaa178b8dd49908a4a4065f8f99a7b13845946a
Chibueze.Nwaka <Chibueze.Nwaka@Nlng.com>
2025-11-03 00:53:50 +0100

    done section 20

--- src/routes/ideas/$ideaId/index.tsx
+++ src/routes/ideas/$ideaId/index.tsx
@@ -5,6 +5,7 @@ import {
   useMutation,
 } from "@tanstack/react-query";
 import { fetchIdea, deleteIdea } from "@/api/ideas";
+import { useAuth } from "@/context/AuthContext";
 
 const ideaQueryOptions = (ideaId: string) =>
   queryOptions({
@@ -24,6 +25,7 @@ function IdeaDetailPage() {
   const { data: idea } = useSuspenseQuery(ideaQueryOptions(ideaId));
 
   const navigate = useNavigate();
+  const { user } = useAuth();
 
   const { mutateAsync: deleteMutateAsync, isPending } = useMutation({
     mutationFn: () => deleteIdea(ideaId),
@@ -51,23 +53,27 @@ function IdeaDetailPage() {
       <h2 className="text-2xl font-bold">{idea.title}</h2>
       <p className="mt-2">{idea.description}</p>
 
-      {/* Edit Link */}
-      <Link
-        to="/ideas/$ideaId/edit"
-        params={{ ideaId: idea._id.toString() }}
-        className="text-sm bg-green-500 text-white mt-4 px-4 py-2 mr-2 rounded inline-block hover:bg-green-600 transition"
-      >
-        Edit
-      </Link>
+      {user && user.id === idea.user && (
+        <>
+          {/* Edit Link */}
+          <Link
+            to="/ideas/$ideaId/edit"
+            params={{ ideaId: idea._id.toString() }}
+            className="text-sm bg-green-500 text-white mt-4 px-4 py-2 mr-2 rounded inline-block hover:bg-green-600 transition"
+          >
+            Edit
+          </Link>
 
-      {/* Delete Button */}
-      <button
-        onClick={handleDelete}
-        disabled={isPending}
-        className="text-sm bg-red-600 text-white mt-4 px-4 py-2 rounded transition disabled:opacity:50"
-      >
-        {isPending ? "Deleting..." : "Delete Idea"}
-      </button>
+          {/* Delete Button */}
+          <button
+            onClick={handleDelete}
+            disabled={isPending}
+            className="text-sm bg-red-600 text-white mt-4 px-4 py-2 rounded transition disabled:opacity:50"
+          >
+            {isPending ? "Deleting..." : "Delete Idea"}
+          </button>
+        </>
+      )}
     </div>
   );
 }
